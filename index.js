2e1439a042d4eda90500ebec3710174785868dd1
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-12-22 15:21:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let dodger = document.getElementById("dodger");
 
 function moveDodgerRight() {
   let leftNumbers = dodger.style.right.replace("px", "");
-  let left = parseInt(rightNumbers, 10);
+  let left = parseInt(leftNumbers, 10);
  
   if (left < 360) {
     dodger.style.left = `${left + 1}px`;

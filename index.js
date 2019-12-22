9f7f265b10a49095174ac2646494a7fe9b745f0d
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-12-22 15:24:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ function moveDodgerRight() {
 }
 
 document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowRight") {
     moveDodgerRight();
   }
